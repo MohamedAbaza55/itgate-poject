4a61893531b29595ae318350de1631bcc71b8f41
MohamedAbaza55 <Abaza2345@gmail.com>
2024-08-19 21:16:05 +0300

    implementing login function

---
+++ login.py
@@ -0,0 +1,14 @@
+#code for FB login
+print("i can login using my fb account")
+
+#code for google login
+print("i can login using my google account")
+
+#code for username and password login
+print("i can login using my username and password account")
+
+#code for github login
+print("i can login using my github account")
+
+#code for instagram login
+print("i can login using my instagram account")
